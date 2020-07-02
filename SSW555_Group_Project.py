43cef5e4af409a3766cc0def79583167462b6152
Alex <apereir3@stevens.edu>
2020-07-01 20:15:39 -0400

    testing travis

--- SSW555_Group_Project.py
+++ SSW555_Group_Project.py
@@ -4,7 +4,7 @@ import datetime
 import os
 from prettytable import PrettyTable
 
-class Family: #comment line to test running Travis CI
+class Family:
     '''class Family'''
     _pretty_table_headers: List[str] = ['ID', 'Married', 'Divorced', 'Husband ID', 'Husband Name', 'Wife ID', 'Wife Name', 'Children']
 
